ffec8a615e3376ca2f474e0534227184ceac5df1
MyriamMonteiro <myriam_job27@hotmail.com>
2024-12-04 08:51:03 -0300

    Myriam

---
+++ script.js
@@ -0,0 +1,5 @@
+const lista = document.getElementById("lista");
+lista.style.color = "DarkOliveGreen";
+
+const Contato = document.getElementById("Contato");
+Contato.style.color = "DarkOliveGreen";
